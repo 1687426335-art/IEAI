84983350d1b9ce60036c9c9d5c51c8b6115f62d3
1687426335-art <1687426335@qq.com>
2026-08-22 21:01:51 +0800

    更新 wyxamzo.lua

--- wyxamzo.lua
+++ wyxamzo.lua
@@ -1030,7 +1030,7 @@ weaponGroup:AddToggle("InfAmmo", {
 
 local mainLeftGroup = Tabs.Player:AddRightGroupbox("快速互动")
 mainLeftGroup:AddToggle("InteractToggle", {
-    Text = "启用互动修改",
+    Text = "启用快速互动（开启后不要动下面的那些默认0就行了）",
     Default = false,
     Callback = function(value)
         interactEnabled = value
