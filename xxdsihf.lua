7fdcb5c1cf30bdc286341e72ecc62c264452074c
1687426335-art <1687426335@qq.com>
2026-09-01 06:01:07 +0800

    更新 xxdsihf.lua

--- xxdsihf.lua
+++ xxdsihf.lua
@@ -406,7 +406,7 @@ function createUI()
     -- ============================================================
     -- 自动躲警察 Tab
     -- ============================================================
-    local PoliceEvadeTab = Window:Tab({ Title = "自动躲警察（如果对面面的因此较高的话还是能给你铐起来）", Icon = "shield" })
+    local PoliceEvadeTab = Window:Tab({ Title = "自动躲警察", Icon = "shield" })
     local PoliceEvadeGroup = PoliceEvadeTab:Section({ Title = "自动躲警察（如果对面面的因此较高的话还是能给你铐起来）", Opened = true })
 
     local AutoEvadePolice = false
