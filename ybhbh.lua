0598ec9355e51f63cbd0f5b5553d6750b10388cf
1687426335-art <1687426335@qq.com>
2026-09-05 15:37:11 +0800

    更新 ybhbh.lua

--- ybhbh.lua
+++ ybhbh.lua
@@ -584,6 +584,21 @@ function createUI()
     AntiFlingLoop()
 
     -- ==================== Tab 创建 ====================
+    -- 作者信息 Tab（第一位）
+    local AuthorTab = Window:Tab({ Title = "作者信息", Icon = "user" })
+    local AuthorSection = AuthorTab:Section({ Title = "", Opened = true })
+    AuthorSection:Paragraph({
+        Title = "",
+        Desc = "",
+        Thumbnail = "rbxassetid://74369447499630",
+        ThumbnailSize = 150,
+        ThumbnailShape = "Rounded",
+    })
+    AuthorSection:Paragraph({
+        Title = "作者QQ：2131869117",
+        Desc = "",
+    })
+
     -- 公告 Tab
     local NoticeTab = Window:Tab({ Title = "公告", Icon = "info" })
     local NoticeSection = NoticeTab:Section({ Title = "作者消息", Opened = true })
@@ -597,19 +612,8 @@ function createUI()
         Desc = "已更换悬浮窗添加了一些功能\n杀戮光环的优先攻击最近目标如果选择距离内没有人\n那这个选项就不会生效杀戮光环正常生效\n请勿将此脚本分享给他人发现我将封禁你的设备\n让你无法使用\n如果你使用的过程中出现一些bug请联系作者修复\n被封永久了就是被挂DC了如果你要是执行其他脚本之后被封的那你也活该"
     })
 
-    -- ==================== 通知 Tab ====================
+    -- 通知 Tab
     local infoTab = Window:Tab({ Title = "通知", Icon = "layout-grid", Locked = false })
-
-    -- 图片（第一位，方形）
-    local imgSection = infoTab:Section({ Title = "", Opened = true })
-    imgSection:Paragraph({
-        Title = "",
-        Desc = "",
-        Thumbnail = "rbxassetid://74369447499630",
-        ThumbnailSize = 99,
-        ThumbnailShape = "Square",
-    })
-
     local infoSection = infoTab:Section({ Title = "详情信息", Icon = "info", Opened = true })
     infoSection:Divider()
     infoSection:Paragraph({
@@ -617,15 +621,13 @@ function createUI()
         Desc = "目前修复了\n使用手机的用户开启飞天卡顿的问题\n目前不知道更新什么功能了\n也没有什么bug了\n有什么功能可以向我提出我会更新",
         ThumbnailSize = 190,
     })
-
     local infoSection2 = infoTab:Section({ Title = "更新公告", Icon = "bell", Opened = true })
     infoSection2:Divider()
     infoSection2:Paragraph({
         Title = "v2.0.4提示",
-        Desc = "修复所有已知问题\n更换了悬浮窗\n新增自动躲警察功能",
+        Desc = "修复所有已知问题\n更换了悬浮窗\n新增自动躲警察功能（含墙体检测）",
         ThumbnailSize = 190,
     })
-
     infoTab:Select()
 
     -- 主功能 Section
