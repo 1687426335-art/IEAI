f61e6ec98214011828562b57152d4b241eb68c57
1687426335-art <1687426335@qq.com>
2026-09-10 11:33:04 +0800

    更新 xxdsihf.lua

--- xxdsihf.lua
+++ xxdsihf.lua
@@ -727,7 +727,7 @@ function createUI()
     infoSection2:Divider()
     infoSection2:Paragraph({
         Title = "v2.0.5提示",
-        Desc = "修复所有已知问题\n更换了悬浮窗\n新增自动躲警察功能",
+        Desc = "修复所有已知问题",
         ThumbnailSize = 190,
     })
     infoTab:Select()
