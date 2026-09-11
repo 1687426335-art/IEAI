320af55fe0bd9f4ac8141b05013ccd69bd827177
1687426335-art <1687426335@qq.com>
2026-09-11 18:46:43 +0800

    更新 ybhbh.lua

--- ybhbh.lua
+++ ybhbh.lua
@@ -82,7 +82,7 @@ function createUI()
     }
 
     local WHITELIST = {
-        ["XXCWYXWFYZDRNGDGHPGRFYDXDACCAD"] = true,
+        ["XXCBYCYCDEAARNGDGHPG"] = true,
         ["XXCWZZCACWARNGDGHPG"] = true,
         ["XXCXXFEXWXARNGDGHPG"] = true,
         ["XWZFFFYAYCRNGDGHPG"] = true,
