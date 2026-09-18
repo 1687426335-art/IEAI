650fbcea6c7d767f77c5e235f5f8a9444ed667d1
wdfex <1687426335@qq.com>
2026-09-19 02:34:20 +0800

    更新 xxdsihf.lua

--- xxdsihf.lua
+++ xxdsihf.lua
@@ -163,7 +163,7 @@ function createUI()
             banner.Size = UDim2.new(0, 220, 0, 28)
             banner.Position = UDim2.new(0, -220, 0, 2)
             banner.BackgroundTransparency = 1
-            banner.Text = "倒卖死爸妈"
+            banner.Text = "91"
             banner.TextSize = 18
             banner.Font = Enum.Font.GothamBold
             banner.TextStrokeTransparency = 0
